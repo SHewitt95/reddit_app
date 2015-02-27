623d306dc40b2f0249e8be143c36dff225fa9869
Sherman Hewitt <s.hewitt7@umiami.edu>
2015-02-27 11:37:41 -0500

    Added try...except to catch user inputs that throw RedirectException

--- reddit.py
+++ reddit.py
@@ -11,7 +11,7 @@ Allow the user to upvote/downvote
 Link to methods: https://praw.readthedocs.org/en/v2.1.20/
 '''
 
-import praw #Import package with reddit library 
+import praw #Import package with reddit library
 	
 if (__name__ == "__main__"):
 	r = praw.Reddit(user_agent='my_cool_application_miami') #Create reddit object
@@ -19,9 +19,16 @@ if (__name__ == "__main__"):
 	'''
 	Account for RedirectException if user inputs invalid subreddit.
 	'''
-	subreddit = raw_input("Please enter name of subreddit: ") #Ask user for name of desired subreddit
-	submissions = r.get_subreddit(subreddit).get_hot(limit=30) #Get first 30 submissions from hot section of subreddit
-	list = [str(item) for item in submissions] #Create list of all found submissions
+	try:
+		#Ask user for name of desired subreddit
+		subreddit = raw_input("Please enter name of subreddit: ") 
+		
+		submissions = r.get_subreddit(subreddit).get_hot(limit=30) #Get first 30 submissions from hot section of subreddit
+		list = [str(item) for item in submissions] #Create list of all found submissions
 
-	for item in list: #Loop through list to print all found submissions
-		print(item)
+		for item in list: #Loop through list to print all found submissions
+			print(item)
+	except:
+		print("The subreddit %s does not exist." %(subreddit))
+		
+		
