827aa1991ccb0f06597ee89e00d37082a42b031f
Sherman Hewitt <s.hewitt7@umiami.edu>
2015-02-26 23:29:39 -0500

    Initial commit

---
+++ reddit.py
@@ -0,0 +1,27 @@
+'''
+-Goals:
+Get the api to work
+Present the 30 posts to the user in a format of your choice
+Allow user input to find specified subreddit
+
+-To go above and beyond:
+Create a gui, and give me a link to the story that opens up in a browser.
+Allow the user to upvote/downvote
+
+Link to methods: https://praw.readthedocs.org/en/v2.1.20/
+'''
+
+import praw #Import package with reddit library 
+	
+if (__name__ == "__main__"):
+	r = praw.Reddit(user_agent='my_cool_application_miami') #Create reddit object
+	
+	'''
+	Account for RedirectException if user inputs invalid subreddit.
+	'''
+	subreddit = raw_input("Please enter name of subreddit: ") #Ask user for name of desired subreddit
+	submissions = r.get_subreddit(subreddit).get_hot(limit=30) #Get first 30 submissions from hot section of subreddit
+	list = [str(item) for item in submissions] #Create list of all found submissions
+
+	for item in list: #Loop through list to print all found submissions
+		print(item)
