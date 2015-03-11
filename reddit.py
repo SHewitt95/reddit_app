fbee694edbe2b41e540c36841962e48bda9f3e5f
Sherman Hewitt <s.hewitt7@umiami.edu>
2015-03-11 17:17:48 -0400

    Encased methods within class Reddit_App.

--- reddit.py
+++ reddit.py
@@ -14,63 +14,70 @@ Link to methods: https://praw.readthedocs.org/en/v2.1.20/
 import praw #Import package with reddit library
 from Tkinter import *
 
-r = praw.Reddit(user_agent='my_cool_application_miami') #Create reddit object
-login = Tk()
-main = Tk()
+class Reddit_App:
 
-login_label_username = Label(login, text="Username:")
-login_label_password = Label(login, text="Password:")
-login_label_username.grid(row=0,column=0)
-login_label_password.grid(row=1,column=0)
+	def __init__(self):
+		r = praw.Reddit(user_agent='my_cool_application_miami') #Create reddit object
+		self.login = Tk()
+		self.login.wm_title("Reddit Login")
+		self.main = Tk()
+		self.main.wm_title("Reddit")
 
-login_username = Entry(login)
-login_password = Entry(login)
-login_username.grid(row=0,column=1)
-login_password.grid(row=1,column=1)
+		self.login_label_username = Label(self.login, text="Username:")
+		self.login_label_password = Label(self.login, text="Password:")
+		self.login_label_username.grid(row=0,column=0)
+		self.login_label_password.grid(row=1,column=0)
 
-login_submit_button = Button(login, text="Submit")
-login_submit_button.grid(row=2,columnspan=2)
+		self.login_username = Entry(self.login)
+		self.login_password = Entry(self.login)
+		self.login_username.grid(row=0,column=1)
+		self.login_password.grid(row=1,column=1)
 
-
-def main():
-	subreddit = getSubreddit()
-	number_of_subred = getNumber("How many topics do you want: ")
-	subreddit = subreddit.get_hot(limit=number_of_subred)
-	try:
-		list = [str(item) for item in subreddit] #Create list of all found submissions
-	except:
-		print("The subreddit does not exist.")
-		quit()
+		self.login_submit_button = Button(self.login, text="Submit")
+		self.login_submit_button.grid(row=2,columnspan=2)
 		
-	for item in list: #Loop through list to print all found submissions
-		print(item)
+		self.login.mainloop()	
+		self.main.mainloop()
+		
+	def main(self):
+		subreddit = getSubreddit()
+		number_of_subred = getNumber("How many topics do you want: ")
+		subreddit = subreddit.get_hot(limit=number_of_subred)
+		try:
+			list = [str(item) for item in subreddit] #Create list of all found submissions
+		except:
+			print("The subreddit does not exist.")
+			quit()
+			
+		for item in list: #Loop through list to print all found submissions
+			print(item)
 	
-def getSubreddit():
-	subreddit = raw_input("Please enter name of subreddit: ") #Ask user for name of desired subreddit
-	submissions = r.get_subreddit(subreddit)
-	
-	return submissions
+	def getSubreddit(self):
+		subreddit = raw_input("Please enter name of subreddit: ") #Ask user for name of desired subreddit
+		submissions = r.get_subreddit(subreddit)
+		
+		return submissions
 
-def getNumber(prompt):
-	# Prompts user for input. Exception/While loop 
-    # catches inputs that aren't proper integers.
-    success = False
-    number = 0
-    while (not success):
-        try:
-            number = int(raw_input(prompt))
-            success = True
-        except:
-            pass
-        
-    return number
+	def getNumber(self, prompt):
+		# Prompts user for input. Exception/While loop 
+		# catches inputs that aren't proper integers.
+		success = False
+		number = 0
+		while (not success):
+			try:
+				number = int(raw_input(prompt))
+				success = True
+			except:
+				pass
+			
+		return number
+
+
+
 
 	
 if (__name__ == "__main__"):
-	main()
-
-login.mainloop()	
-main.mainloop()
+	Reddit_App()
 	
 '''
 -Buttons on GUI can be equivalent to numbers in dictionary-as-switch.
